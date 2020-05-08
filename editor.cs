ab56233c0eeb938c4fa3fceff9cd0eaf1d1931a9
dHivya6 <65015791+dHivya6@users.noreply.github.com>
2020-05-08 18:12:58 +0530

    create editor.css

---
+++ editor.cs
@@ -0,0 +1,27 @@
+canvas {
+  margin: auto;
+  background: #ececec;
+  width: 100%;
+}
+body{
+  background-image:url("https://images.pexels.com/photos/1167021/pexels-photo-1167021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+}
+.btn-disabled {
+  color: #fff !important;
+}
+.btn-active{
+  color:#66bfbf !important;
+}
+.col-md-5{
+  margin:3px 3px;
+  padding:2px;
+}
+#download-btn{
+  margin-bottom: 5px;
+}
+#revert-btn{
+  margin-bottom:5px;
+}
+h4{
+  color:#555555;
+}
